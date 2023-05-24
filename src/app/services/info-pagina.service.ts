c66f39276e1a3b2c37194c0eb99c585922cffc58
dfarana <dfarana@alumina.com>
2023-05-24 08:02:32 -0500

    Fin Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../Interfaces/info-pagina.interface';
+import { Equipo } from '../Interfaces/equipo-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,41 @@ export class InfoPaginaService {
   info:InfoPagina = {};
   cargada:boolean = false;
 
+  equipo:any[]  = [];
+
   constructor(private http: HttpClient) {
     //console.log('Servicio de infoPagina Listo');
 
     // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+    /* this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
-
       this.cargada = true;
       this.info = resp;
-
       //console.log(resp);
       //console.log( resp.email);
-    });
+    }); */
+    this.cargarInfo();
+    this.cargarEquipo();
 
-   }
+  }
+
+  private cargarInfo(){
+
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+      //console.log(resp);
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-f0180-default-rtdb.firebaseio.com/Equipo.json')
+    .subscribe((resp: any) => {
+      this.cargada = true;
+      this.equipo = resp;
+      //console.log(resp);
+    });
+  }
+
 }
